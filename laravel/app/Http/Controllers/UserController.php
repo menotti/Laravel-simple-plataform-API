00026888c94476847f7521e19cd0fc4ef5506ed9
MarlonHenq <marlon@marlonhenq.dev>
2024-05-29 02:02:33 -0300

    add user controler

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json(User::all());
+    }
+
+    public function show($id)
+    {
+        return response()->json(User::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|string|email|max:255|unique:users,email,' . $id,
+            'password' => 'sometimes|required|string|min:8',
+        ]);
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->has('email')) {
+            $user->email = $request->email;
+        }
+
+        if ($request->has('password')) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->save();
+
+        return response()->json($user);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted']);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\UserController;
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Route::middleware('auth:sanctum')->group(function () {
+//    Route::get('users', [UserController::class, 'index']);
+//    Route::get('users/{id}', [UserController::class, 'show']);
+//    Route::put('users/{id}', [UserController::class, 'update']);
+//    Route::delete('users/{id}', [UserController::class, 'destroy']);
+//});
